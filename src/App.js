7145ea492fd51ed294aac6230ad604ee9eae28c0
Dan Florio <dflo@danflorio.com>
2018-09-25 12:52:23 -0400

    add info bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Game from './parts/Game';
+import InfoBar from './parts/InfoBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <InfoBar />
         <Game />
       </div>
     );
---
+++ src/parts/InfoBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import logo from '../logo.svg';
+
+
+const InfoBar = () => (
+  <div className="info-bar">
+    <img src={logo} className="info-bar__logo" />
+    <div className="info-bar__text">
+      <h1 className="info-bar__title">
+        React Mastermind
+      </h1>
+      <h2 className="info-bar__subtitle">
+        by <a href="http://www.danflorio.com" className="info-bar__link">Dan Florio</a>
+      </h2>
+    </div>
+  </div>
+);
+
+export default InfoBar;
